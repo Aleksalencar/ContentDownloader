1452ef125658a79ccbb4fdd7d0ef996a12ba9b16
Aleksalencar <47511195+Aleksalencar@users.noreply.github.com>
2020-01-16 23:20:04 -0300

    Web crawling na pagina de do Anime

--- ZippyShare/Zippy.py
+++ ZippyShare/Zippy.py
@@ -1,23 +1,33 @@
 from selenium import webdriver
+driver = webdriver.Chrome('C:/Users/Aleksander/PycharmProjects/AnimeDownloader/webdriver/chromedriver.exe')
 
-
-def download(addres):
-    chrome.get(addres)
-    dlbutton = chrome.find_elements_by_id('dlbutton')
+def zippidownload(addres):
+    driver.get(addres)
+    driver.implicitly_wait(10)
+    dlbutton = driver.find_elements_by_id('dlbutton')
     dl = dlbutton[0].get_attribute('href')
     print(dl)
-    chrome.get(dl)
+    driver.get(dl)
     pass
 
 
 
-
-episode_list = input()
+driver.get('https://www.anbient.com/anime/hibike-euphonium-2')
+elements = driver.find_elements_by_tag_name('a')
+episode_list = []
+for element in elements:
+    episode = element.get_attribute('href')
+    if episode is not None and 'zippyshare.com/' in episode:
+        episode_list.append(episode)
+        pass
+    pass
 print(episode_list)
-chrome = webdriver.Chrome('C:/Users/Aleksander/PycharmProjects/AnimeDownloader/webdriver/chromedriver.exe')
 for episode in episode_list:
    try:
-    download(episode)
-    chrome.implicitly_wait(10)
+    zippidownload(episode)
+    pass
    except:
        print("fail")
+       pass
+   pass
+driver.get('chrome://downloads')
